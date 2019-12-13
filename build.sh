4debe72427063a8944c29d30f6da7eed77c59d6b
Martin Costello <martin@martincostello.com>
2019-12-13 16:31:26 +0000

    Update to ASP.NET Core 3.1 (#49)
    * Update to ASP.NET Core 3.1
    
    Update to ASP.NET Core 3.1.0.
    Update NuGet packages to their latest versions.
    
    * Fix-up build script
    
    Apply various improvements from other repos, which should also fix the tests failing in AppVeyor.
    
    * Update .travis.yml
    
    Rely on build.sh to install .NET Core.
    
    * Update build.sh
    
    Do not install 2.2 to a custom directory.
    Use the most recent release of the 2.2 SDK.
    
    * Update build.sh
    
    Swap SDK install order to try and fix the build.
    
    * Install .NET 2.2 in AppVeyor
    
    Install .NET Core 2.2 in AppVeyor to try and workaround the CI build issue causing BundleMinifier.Core to fail.
    
    * Update Selenium.WebDriver.ChromeDriver
    
    Update to the latest release of Selenium.WebDriver.ChromeDriver.

--- build.sh
+++ build.sh
@@ -31,7 +31,7 @@ export PATH="$DOTNET_INSTALL_DIR:$PATH"
 dotnet_version=$(dotnet --version)
 
 if [ "$dotnet_version" != "$CLI_VERSION" ]; then
-    curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --version "2.2.402" --install-dir "$DOTNET_INSTALL_DIR"
+    curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --version "2.2.207" --install-dir "$DOTNET_INSTALL_DIR"
     curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --version "$CLI_VERSION" --install-dir "$DOTNET_INSTALL_DIR"
 fi
 
