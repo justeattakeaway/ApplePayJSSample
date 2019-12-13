25447ec4b5d1556953678d72e8e046aadef33105
martincostello <martin@martincostello.com>
2019-12-13 16:17:13 +0000

    Install .NET 2.2 in AppVeyor
    Install .NET Core 2.2 in AppVeyor to try and workaround the CI build issue causing BundleMinifier.Core to fail.

--- Build.ps1
+++ Build.ps1
@@ -53,6 +53,7 @@ if ($installDotNetSdk -eq $true) {
         $installScript = Join-Path $env:DOTNET_INSTALL_DIR "install.ps1"
         [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor "Tls12"
         Invoke-WebRequest "https://dot.net/v1/dotnet-install.ps1" -OutFile $installScript -UseBasicParsing
+        & $installScript -Version "2.2.207" -InstallDir "$env:DOTNET_INSTALL_DIR" -NoPath
         & $installScript -Version "$dotnetVersion" -InstallDir "$env:DOTNET_INSTALL_DIR" -NoPath
     }
 }
